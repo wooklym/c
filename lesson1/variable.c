2759466d30bceccc6c64a8d12a4c141aa096b60b
wooklym <wooklym@gmail.com>
2016-12-04 21:57:00 +0900

    add variable example

---
+++ lesson1/variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int a;
+  a = 0;
+
+  int b = 1;
+
+  printf("a is %d, b is %d\n", a, b);
+
+  a = 3;
+
+  printf("a is %d, b is %d\n", a, b);
+
+  return 0;
+}
